60720ba08d8ee68943a1c84245b23aec2f3ab6c9
jrms <fake@email.com>
2024-05-11 12:35:31 -0700

    Install Provider as stage manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/pages.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     var themeData = ThemeData(
@@ -16,15 +19,18 @@ class MyApp extends StatelessWidget {
       fontFamily: 'OpenSans',
     );
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR Reader',
-      initialRoute: 'home',
-      routes: {
-        '/': (_) => const HomePage(),
-        'map': (_) => const MapPage(),
-      },
-      theme: themeData,
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Reader',
+        initialRoute: 'home',
+        routes: {
+          '/': (_) => const HomePage(),
+          'map': (_) => const MapPage(),
+        },
+        theme: themeData,
+      ),
     );
   }
 }
