9be45ade5fc812b7a625fafb20a5a5c3b82826c3
jrms <fake@email.com>
2024-05-11 11:03:15 -0700

    Add themedata

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,25 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'QR Reader', initialRoute: 'home', routes: {
-      '/': (_) => const HomePage(),
-      'map': (_) => const MapPage(),
-    });
+    var themeData = ThemeData(
+      primarySwatch: Colors.blue,
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Colors.blueAccent,
+      ),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      appBarTheme: const AppBarTheme(elevation: 0),
+      fontFamily: 'OpenSans',
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        '/': (_) => const HomePage(),
+        'map': (_) => const MapPage(),
+      },
+      theme: themeData,
+    );
   }
 }
