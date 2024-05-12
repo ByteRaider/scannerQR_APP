fc8c0b3f39d6e034e576e4a8fb434673c198ff9f
jrms <fake@email.com>
2024-05-12 09:21:06 -0700

    Add dependencies: path_provider & SQLite

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -7,7 +8,15 @@ class ScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancel', false, ScanMode.QR);
+        print(barcodeScanRes);
+        if (barcodeScanRes == '-1') {
+          return;
+        }
+        // Navigator.pushNamed(context, 'map', arguments: barcodeScanRes);
+      },
       child: const Icon(Icons.filter_center_focus),
     );
   }
