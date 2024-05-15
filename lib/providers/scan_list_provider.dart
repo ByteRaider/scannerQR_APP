700aa9407704863fb351a3b2ea87f18ddf1cbe71
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-05-15 14:15:48 -0700

    Add methods: loadScans, loadScansByType, deleteAllScans to ScanListProvider

--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -18,4 +18,35 @@ class ScanListProvider extends ChangeNotifier {
       notifyListeners();
     }
   }
+
+  loadScans() async {
+    // Get all scans
+    final scans = await DBProvider.db.getScansAll();
+    // Set List<ScanModel> scans = to final scans list with spread operator
+    this.scans = [...scans];
+    notifyListeners();
+  }
+
+  loadScansByType(String tipo) async {
+    final scans = await DBProvider.db.getScansByType(tipo);
+    ''' create a new list scans with the same elements as the scans list obtained from
+        the database and assigns it to the scans property of the current object.
+    ''';
+    // Set List<ScanModel> scans = to final scans list with spread operator
+    this.scans = [...scans];
+    selectedType = tipo;
+    notifyListeners();
+  }
+
+  deleteAllScans() async {
+    await DBProvider.db.deleteAllScans();
+    scans = [];
+    notifyListeners();
+  }
+
+  deleteScan(int id) async {
+    await DBProvider.db.deleteScan(id);
+    loadScansByType(selectedType);
+    //notifyListeners(); is loaded by loadScansByType
+  }
 }
