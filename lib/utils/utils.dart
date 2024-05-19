4329ccff1976a3610fa6c8dda01fd5a6575ebc21
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-05-18 20:07:33 -0700

    Document launchURL

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,21 +3,19 @@ import 'package:qr_reader/models/models.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 launchURL(BuildContext context, ScanModel scan) async {
-  final Uri url = Uri(
-    scheme: 'http',
-    host: 'dart.dev',
-    path: '/guides/libraries/library-tour',
-  );
+  // Parse from String to Uri object in order to launch the URL
+  final Uri url = Uri.parse(scan.valor);
 
   if (scan.tipo == 'http') {
+    // Checks whether the specified URL can be handled by some app installed on the device.
     if (await canLaunchUrl(url)) {
-      print('LAUNCHING URL: $url');
+      // Opens the URL in the system's default browser.
       await launchUrl(url, mode: LaunchMode.platformDefault);
     } else {
       throw 'Something went wrong while launching URL: $url';
     }
   } else {
-    print('Scan tipo es: ${scan.tipo}');
+    // Maps
     Navigator.pushNamed(context, 'map', arguments: scan);
   }
 }
